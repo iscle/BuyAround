5fe1e9ab60e7b694bc25c56ff3fe814afd70b2cf
Iscle <albertiscle9@gmail.com>
2020-04-08 01:35:02 +0200

    Use ViewBinding for RegisterFragment

--- app/src/main/java/com/selepdf/hackovid/fragment/RegisterFragment.java
+++ app/src/main/java/com/selepdf/hackovid/fragment/RegisterFragment.java
@@ -9,13 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.selepdf.hackovid.R;
+import com.selepdf.hackovid.databinding.FragmentRegisterBinding;
 
 public class RegisterFragment extends Fragment {
+
+    private FragmentRegisterBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
