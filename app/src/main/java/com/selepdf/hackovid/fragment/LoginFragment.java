1449131483405d45a0b21ee9c3bc2fb12cef8f5c
Ferran <ferran.montoliu@students.salle.url.edu>
2020-04-08 18:46:32 +0200

    Started to implement login and register fragments

--- app/src/main/java/com/selepdf/hackovid/fragment/LoginFragment.java
+++ app/src/main/java/com/selepdf/hackovid/fragment/LoginFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +12,13 @@ import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
 import com.selepdf.hackovid.R;
+import com.selepdf.hackovid.callback.LoginCallback;
 import com.selepdf.hackovid.databinding.FragmentLoginBinding;
+import com.selepdf.hackovid.model.UserType;
 
 import dagger.android.support.DaggerFragment;
 
-public class LoginFragment extends DaggerFragment {
+public class LoginFragment extends DaggerFragment implements LoginCallback {
 
     private FragmentLoginBinding binding;
 
@@ -30,10 +33,34 @@ public class LoginFragment extends DaggerFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button).setOnClickListener(v -> {
+        view.findViewById(R.id.login_login_to_register).setOnClickListener(v -> {
             NavDirections action = LoginFragmentDirections
                     .actionLoginFragmentToRegisterFragment();
             Navigation.findNavController(v).navigate(action);
         });
+
+        view.findViewById(R.id.login_btn_login).setOnClickListener(v -> {
+            // TODO: API CALL TO LOG IN
+        });
+    }
+
+    @Override
+    public void onSuccess(UserType userType) {
+        // TODO: GO TO MAIN FRAGMENT
+    }
+
+    @Override
+    public void onFailure(LoginError loginError) {
+        switch (loginError) {
+            case NETWORK_ERROR:
+                Toast.makeText(getContext(), R.string.network_error, Toast.LENGTH_LONG).show();
+                break;
+            case INTERNAL_ERROR:
+                Toast.makeText(getContext(), R.string.internal_error, Toast.LENGTH_LONG).show();
+                break;
+            case WRONG_PASSWORD:
+                Toast.makeText(getContext(), R.string.wrong_password, Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
