cfb3afc4f8b8948b8943afeaaeb2fd0c8edebc63
Jcarri14 <j.carrion@students.salle.url.edu>
2020-04-10 01:42:45 +0200

    Create AccountViewModel.java

---
+++ app/src/main/java/com/selepdf/hackovid/viewmodel/AccountViewModel.java
@@ -0,0 +1,30 @@
+package com.selepdf.hackovid.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.selepdf.hackovid.model.User;
+import com.selepdf.hackovid.repository.HackovidRepository;
+
+import javax.inject.Inject;
+
+public class AccountViewModel extends ViewModel {
+
+    private HackovidRepository hackovidRepository;
+    private MutableLiveData<User> mUser;
+
+    @Inject
+    public AccountViewModel(HackovidRepository hackovidRepository) {
+        this.hackovidRepository = hackovidRepository;
+        mUser = new MutableLiveData<>();
+    }
+
+    public void requestUserInfoByName(String name) {
+        // Handle request
+    }
+
+    public LiveData<User> getUser() {
+        return mUser;
+    }
+}
