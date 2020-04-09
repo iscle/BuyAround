0440737ba157ca27aecd1811c1feea013d65ae50
Jcarri14 <j.carrion@students.salle.url.edu>
2020-04-09 04:04:51 +0200

    Create SearchViewModel.java

---
+++ app/src/main/java/com/selepdf/hackovid/viewmodel/SearchViewModel.java
@@ -0,0 +1,25 @@
+package com.selepdf.hackovid.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+import com.selepdf.hackovid.callback.SearchCallback;
+import com.selepdf.hackovid.repository.HackovidRepository;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+public class SearchViewModel extends ViewModel {
+
+    private HackovidRepository hackovidRepository;
+
+    @Inject
+    public SearchViewModel(HackovidRepository hackovidRepository) {
+        this.hackovidRepository = hackovidRepository;
+    }
+
+    public void search(String input, SearchCallback searchCallback) {
+        searchCallback.onSearch(new ArrayList<>());
+    }
+
+}
