7bb8f8ad65986ee3c33167009dba5fe2ddf48def
Jcarri14 <j.carrion@students.salle.url.edu>
2020-04-09 04:05:04 +0200

    Update FragmentModule.java

--- app/src/main/java/com/selepdf/hackovid/di/module/FragmentModule.java
+++ app/src/main/java/com/selepdf/hackovid/di/module/FragmentModule.java
@@ -1,7 +1,13 @@
 package com.selepdf.hackovid.di.module;
 
+import android.accounts.Account;
+
+import com.selepdf.hackovid.fragment.AccountFragment;
+import com.selepdf.hackovid.fragment.HomeFragment;
 import com.selepdf.hackovid.fragment.LoginFragment;
+import com.selepdf.hackovid.fragment.OrdersFragment;
 import com.selepdf.hackovid.fragment.RegisterFragment;
+import com.selepdf.hackovid.fragment.SearchFragment;
 
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
@@ -14,5 +20,15 @@ public abstract class FragmentModule {
 
     @ContributesAndroidInjector
     abstract LoginFragment loginFragment();
+
+    @ContributesAndroidInjector
+    abstract HomeFragment homeFragment();
+
+    @ContributesAndroidInjector
+    abstract SearchFragment searchFragment();
+
+    @ContributesAndroidInjector
+    abstract OrdersFragment ordersFragment();
+
 
 }
