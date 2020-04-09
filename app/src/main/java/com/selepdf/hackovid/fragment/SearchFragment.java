6fe53fa3f656ebed64136b1198f75361197ccbab
Jcarri14 <j.carrion@students.salle.url.edu>
2020-04-09 04:04:57 +0200

    Create SearchFragment.java

---
+++ app/src/main/java/com/selepdf/hackovid/fragment/SearchFragment.java
@@ -0,0 +1,35 @@
+package com.selepdf.hackovid.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.navigation.Navigation;
+
+import com.selepdf.hackovid.databinding.FragmentRegisterBinding;
+import com.selepdf.hackovid.databinding.FragmentSearchBinding;
+
+import dagger.android.support.DaggerFragment;
+
+public class SearchFragment extends DaggerFragment {
+
+    private FragmentSearchBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+}
