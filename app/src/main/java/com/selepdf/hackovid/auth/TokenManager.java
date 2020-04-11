8228fb168335d45d0135683f5061b3892bd066ea
Iscle <albertiscle9@gmail.com>
2020-04-11 03:43:00 +0200

    Improved token management

--- app/src/main/java/com/selepdf/hackovid/auth/TokenManager.java
+++ app/src/main/java/com/selepdf/hackovid/auth/TokenManager.java
@@ -11,20 +11,27 @@ public class TokenManager {
     private static final String TOKEN_PREF = "token";
 
     private final SharedPreferences sharedPreferences;
+    private String token;
 
     @Inject
     public TokenManager(Context context) {
         this.sharedPreferences = context.getSharedPreferences(TOKEN_MANAGER_PREF_NAME, Context.MODE_PRIVATE);
+        this.token = null;
     }
 
     public void setToken(String token) {
+        this.token = token;
         SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
         sharedPrefEditor.putString(TOKEN_PREF, token);
         sharedPrefEditor.apply();
     }
 
     public String getToken() {
-        return sharedPreferences.getString(TOKEN_PREF, "");
+        if (token == null) {
+            token = sharedPreferences.getString(TOKEN_PREF, "");
+        }
+
+        return token;
     }
 
     public static boolean isTokenValid(String token) {
