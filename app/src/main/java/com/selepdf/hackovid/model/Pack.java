e10a45de4d1c2f00ac22f5714cc04410b26f5518
Jcarri14 <j.carrion@students.salle.url.edu>
2020-04-11 12:35:18 +0200

    Update: class models Pack, Product ( not extending from other class)

--- app/src/main/java/com/selepdf/hackovid/model/Pack.java
+++ app/src/main/java/com/selepdf/hackovid/model/Pack.java
@@ -1,12 +1,71 @@
 package com.selepdf.hackovid.model;
 
-public class Pack extends GeneralItem{
+public class Pack {
 
+    private String name;
+    private String description;
+    private String thumbnail;
+    private float price;
+    private float rating;
+    private String[] images;
     private Product[] products;
 
     public Pack(String name, String description, String thumbnail, float price, float rating, String[] images, Product[] products) {
-        super(name, description, thumbnail, price, rating, images);
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+        this.price = price;
+        this.rating = rating;
+        this.images = images;
         this.products = products;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String[] getImages() {
+        return images;
+    }
+
+    public void setImages(String[] images) {
+        this.images = images;
     }
 
     public Product[] getProducts() {
--- app/src/main/java/com/selepdf/hackovid/model/Product.java
+++ app/src/main/java/com/selepdf/hackovid/model/Product.java
@@ -1,18 +1,73 @@
 package com.selepdf.hackovid.model;
 
-public class Product extends GeneralItem {
+public class Product {
 
+    private String name;
+    private String description;
+    private String thumbnail;
+    private float price;
+    private float rating;
+    private String[] images;
     private Category category;
     private Store store;
 
     public Product(String name, String description, String thumbnail, float price, float rating, String[] images, Category category, Store store) {
-        super(name, description, thumbnail, price, rating, images);
+        this.name = name;
+        this.description = description;
+        this.thumbnail = thumbnail;
+        this.price = price;
+        this.rating = rating;
+        this.images = images;
         this.category = category;
         this.store = store;
     }
 
-    public Product(String name, String description, String price) {
-        super(name, description, price);
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String[] getImages() {
+        return images;
+    }
+
+    public void setImages(String[] images) {
+        this.images = images;
     }
 
     public Category getCategory() {
