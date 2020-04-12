d4721e5f8b3e685f4d9e65a17558eb1e2bbb9197
Jcarri14 <j.carrion@students.salle.url.edu>
2020-04-12 20:55:57 +0200

    Create PayFragment.java

---
+++ app/src/main/java/com/selepdf/buyaround/fragment/PayFragment.java
@@ -0,0 +1,30 @@
+package com.selepdf.buyaround.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.selepdf.buyaround.databinding.FragmentPayBinding;
+
+import dagger.android.support.DaggerFragment;
+
+public class PayFragment extends DaggerFragment {
+
+    private FragmentPayBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentPayBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
