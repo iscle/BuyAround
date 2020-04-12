ba2283865a5f64175b503fcd84b056c95a5fcbfa
Jcarri14 <j.carrion@students.salle.url.edu>
2020-04-12 22:32:08 +0200

    Update CartFragment.java

--- app/src/main/java/com/selepdf/buyaround/fragment/CartFragment.java
+++ app/src/main/java/com/selepdf/buyaround/fragment/CartFragment.java
@@ -12,6 +12,7 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.UserManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -53,7 +54,7 @@ public class CartFragment extends DaggerFragment implements IListAdapter {
         super.onViewCreated(view, savedInstanceState);
 
         cartViewModel = new ViewModelProvider(this, viewModelFactory).get(CartViewModel.class);
-
+        subscribeObservers();
         binding.cartItemsRecyclerview.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
         binding.cartToPayBtn.setOnClickListener(v -> {
             NavDirections action = CartFragmentDirections
@@ -66,7 +67,7 @@ public class CartFragment extends DaggerFragment implements IListAdapter {
     }
 
     private void subscribeObservers() {
-        cartViewModel.getOrderProducts().observe(this, new Observer<List<OrderProduct>>() {
+        cartViewModel.getOrderProducts().observe(getViewLifecycleOwner(), new Observer<List<OrderProduct>>() {
             @Override
             public void onChanged(List<OrderProduct> products) {
                 binding.cartItemsRecyclerview.setAdapter(new CartItemsAdapter(products, CartFragment.this));
