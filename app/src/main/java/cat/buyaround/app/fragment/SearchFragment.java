17d2b6afd521041fcffdbbbb68dbdd5a49e7c948
Ferran <ferran.montoliu@students.salle.url.edu>
2020-06-14 02:49:19 +0200

    Search fragment opens keyboard on open + visual refactor (WIP)

--- app/src/main/java/cat/buyaround/app/fragment/SearchFragment.java
+++ app/src/main/java/cat/buyaround/app/fragment/SearchFragment.java
@@ -1,20 +1,31 @@
 package cat.buyaround.app.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import javax.inject.Inject;
 
 import cat.buyaround.app.databinding.FragmentSearchBinding;
 
+import cat.buyaround.app.factory.ViewModelFactory;
+import cat.buyaround.app.viewmodel.SearchViewModel;
 import dagger.android.support.DaggerFragment;
 
 public class SearchFragment extends DaggerFragment {
 
     private FragmentSearchBinding binding;
+
+    @Inject
+    protected ViewModelFactory viewModelFactory;
+    private SearchViewModel searchViewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,7 +38,15 @@ public class SearchFragment extends DaggerFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        searchViewModel = new ViewModelProvider(this, viewModelFactory).get(SearchViewModel.class);
 
+        initView();
+    }
+
+    private void initView() {
+        binding.searchEt.requestFocus();
+        ((InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE))
+                .showSoftInput(binding.searchEt, InputMethodManager.SHOW_IMPLICIT);
     }
 
 }
