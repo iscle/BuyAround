d9f7da8154bb75a05b571723280d6d13b22157ce
Iscle <albertiscle9@gmail.com>
2020-06-14 14:12:36 +0200

    Fix back toolbar arrow when using bottom navigation

--- app/src/main/java/cat/buyaround/app/activity/MainActivity.java
+++ app/src/main/java/cat/buyaround/app/activity/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.inject.Inject;
 
@@ -30,7 +34,13 @@ public class MainActivity extends DaggerAppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
         // Set up toolbar
-        NavigationUI.setupWithNavController(binding.toolbar, navController);
+        Set<Integer> tlds = new HashSet<>();
+        tlds.add(R.id.homeFragment);
+        tlds.add(R.id.categoriesFragment);
+        tlds.add(R.id.favouritesFragment);
+        tlds.add(R.id.accountFragment);
+        NavigationUI.setupWithNavController(binding.toolbar, navController,
+                new AppBarConfiguration.Builder(tlds).build());
 
         // Set up bottom navigation
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
